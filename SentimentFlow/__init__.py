f3d60327b6f5683f4eb6429fd4ed844e49f07765
Davide <davide.picca@unil.ch>
2024-05-21 16:57:22 +0200

    Update __init__.py

--- SentimentFlow/__init__.py
+++ SentimentFlow/__init__.py
@@ -1,3 +1,3 @@
 from .data_processing import SpeechProcessor
 from .senti_keywords import KeywordAnalyzer
-from .sentiment_analysis import SentimentAnalyzer
+from .sentiment_analysis import SentimentFlowCalculator
